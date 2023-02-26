9d0cf90d583fc91cbf64ee4e2b66d2baacc142e1
akdjhdfhfhd <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-02-26 11:56:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "5000"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/b4b6f2afd7faac0013c79.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/b4b6f2afd7faac0013c79.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/22394c2ebaf52272be4da.jpg")
+START_IMG = getenv("START_IMG", "https://te.legra.ph/file/22394c2ebaf52272be4da.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ah07v")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ah_9_v")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ah07v")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5656828413").split()))
 
 
-FAILED = "https://te.legra.ph/file/b4b6f2afd7faac0013c79.jpg"
+FAILED = "https://telegra.ph/file/22394c2ebaf52272be4da.jpg"
